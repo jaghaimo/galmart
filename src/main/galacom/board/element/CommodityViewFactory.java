732ef2846721969fa1cf33c832aeba9b5e01d0aa
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2020-12-01 23:26:52 +0000

    Simplify factory

--- src/main/galacom/board/element/CommodityViewFactory.java
+++ src/main/galacom/board/element/CommodityViewFactory.java
@@ -5,12 +5,12 @@ import com.fs.starfarer.api.campaign.econ.CommoditySpecAPI;
 import com.fs.starfarer.api.campaign.econ.EconomyAPI;
 
 import galacom.extractor.BuyFromMarketExtractor;
-import galacom.extractor.MarketExtractor;
 import galacom.extractor.SellOnMarketExtractor;
 import galacom.ui.Header;
 import galacom.ui.Renderable;
 import galacom.ui.Stack;
 import galacom.ui.Table;
+import galacom.ui.TableContent;
 
 public class CommodityViewFactory {
 
@@ -24,19 +24,12 @@ public class CommodityViewFactory {
         String commodityName = getCommodityName(commodityId);
         float labelHeight = 25f;
         float tableHeight = (height / 2) - labelHeight;
+        TableContent buyTableContent = new BuyFromMarketExtractor(commodityId, economy);
+        TableContent sellTableContent = new SellOnMarketExtractor(commodityId, economy);
         return new Stack(new Header("Buy Price for 100 " + commodityName, width),
-                getBuyTable(commodityId, width, tableHeight), new Header("Sell Price for 100 " + commodityName, width),
-                getSellTable(commodityId, width, tableHeight));
-    }
-
-    private Table getBuyTable(String commodityId, float width, float height) {
-        MarketExtractor marketExtractor = new BuyFromMarketExtractor(commodityId, economy);
-        return new Table(getCommodityName(commodityId), width, height, marketExtractor);
-    }
-
-    private Table getSellTable(String commodityId, float width, float height) {
-        MarketExtractor marketExtractor = new SellOnMarketExtractor(commodityId, economy);
-        return new Table(getCommodityName(commodityId), width, height, marketExtractor);
+                new Table(commodityId, width, tableHeight, buyTableContent),
+                new Header("Sell Price for 100 " + commodityName, width),
+                new Table(commodityId, width, tableHeight, sellTableContent));
     }
 
     private String getCommodityName(String commodityId) {
